0b7784d21c1d3a7ded38105c79f92b6fac99e42a
OlegSotnik <oleg-00@yandex.ru>
2022-07-04 15:24:56 +0400

    оживил модальное окно обратной связи

--- source/js/script.js
+++ source/js/script.js
@@ -12,12 +12,28 @@ headerToggle.addEventListener("click", function (evt) {
   headerNavList.classList.toggle("header__nav-list--closed");
 });
 
-document.addEventListener( 'click', (e) => {
-	const withinBoundariesList = e.composedPath().includes(headerNavList);
+document.addEventListener('click', (e) => {
+  const withinBoundariesList = e.composedPath().includes(headerNavList);
   const withinBoundariesToggle = e.composedPath().includes(headerToggle);
 
-	if ( ! withinBoundariesList && ! withinBoundariesToggle  ) {
+  if (!withinBoundariesList && !withinBoundariesToggle) {
     headerNavList.classList.add("header__nav-list--closed");
     headerToggle.classList.remove("header__toggle--closed");
-	}
+  }
 })
+
+// Модальное окно кнопки призыва к действию
+
+const modalCTA = document.querySelector(".page__modal-call-to-action");
+const buttonCTA = document.querySelector(".call-to-action__button");
+const toggleCTA = document.querySelector(".modal-call-to-action__toggle-box, .modal-call-to-action__toggle");
+
+buttonCTA.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalCTA.classList.remove("visually-hidden");
+});
+
+toggleCTA.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalCTA.classList.add("visually-hidden");
+});
