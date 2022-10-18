434c6b470194f88eb39f11f4766befd0b812c03c
OlegSotnik <oleg-00@yandex.ru>
2022-10-18 11:24:37 +0400

    написал валидацию полей контактов модульного окна

--- source/js/script.js
+++ source/js/script.js
@@ -37,3 +37,78 @@ toggleCTA.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalCTA.classList.add("visually-hidden");
 });
+
+// Проверяем валидация формы
+
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.modal-call-to-action__form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+
+    if (error <= 4) {
+
+    } else {
+      alert('заполните минимум одно поле с контактными данными!');
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('.item-radio__input-field');
+
+    for (let i = 0; i < formReq.length; i++) {
+      const input = formReq[i];
+      formRemoveError(input);
+
+      if (input.classList.contains('__email')) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else if (input.classList.contains('__phone')) {
+        if (phoneTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else if (input.classList.contains('__telegram')) {
+        if (telegramTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    input.classList.add('item-radio__input-field--error');
+  }
+
+  function formRemoveError(input) {
+    input.classList.remove('item-radio__input-field--error');
+  }
+
+  // функция валидации email
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
+
+  // функция валидации phone
+  function phoneTest(input) {
+    return !/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(input.value);
+  }
+
+  // функция валидации telegram
+  function telegramTest(input) {
+    return !/^@\w+([\.-]?\w+)$/.test(input.value);
+  }
+
+});
+
+// Отправка сообщения на почту
