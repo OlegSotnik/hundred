aabf82a88c8ab89c4425f645bd2a696cface328f
OlegSotnik <oleg-00@yandex.ru>
2022-11-19 23:39:20 +0400

    оптимизация

--- source/js/script.js
+++ source/js/script.js
@@ -315,21 +315,16 @@ onP();
 
 document.addEventListener('touchstart', handleTouchStart, false);
 document.addEventListener('touchmove', handleTouchMove, false);
-// document.addEventListener('touchend', handleTouchEnd, false);
 
 const sliderList = document.querySelector('.feedback__slider-list');
 
 let x1 = null;
-// let y1 = null;
 
 function handleTouchStart(event) {
   const firstTouch = event.touches[0];
 
   x1 = firstTouch.clientX;
-  // y1 = firstTouch.clientY;
 
-
-  // console.log(x1, y1);
 }
 
 function handleTouchMove(event) {
@@ -338,10 +333,8 @@ function handleTouchMove(event) {
   }
 
   let x2 = event.touches[0].clientX;
-  // let y2 = event.touches[0].clientY;
   let xDiff = x2 - x1;
-  // let yDiff = y2 - y1;
-  // console.log(x2, y2);
+
   if (xDiff > 7) {
     for (let index = 0; index < sliderItems.length; index++) {
       if (radioButtons[index].checked) {
@@ -362,83 +355,3 @@ function handleTouchMove(event) {
   x1 = null;
   y1 = null;
 }
-
-////
-// let sliderTrack = document.querySelector('.feedback__slider-list');
-// posInit = 0;
-// posX1 = 0;
-// posX2 = 0;
-// posFinal = 0;
-// posThreshold = slideWidth * .35;
-// trfRegExp = /[-0-9.]+(?=px)/;
-
-// getEvent = function () {
-//   return event.type.search('touch') !== -1 ? event.touches[0] : event;
-//   // p.s. event - аргумент по умолчанию в функции
-// },
-//   // или es6
-//   getEvent = () => event.type.search('touch') !== -1 ? event.touches[0] : event,
-
-//   swipeStart = function () {
-//     let evt = getEvent();
-
-//     // берем начальную позицию курсора по оси Х
-//     posInit = posX1 = evt.clientX;
-
-//     // убираем плавный переход, чтобы track двигался за курсором без задержки
-//     // т.к. он будет включается в функции slide()
-//     sliderTrack.style.transition = '';
-
-//     // и сразу начинаем отслеживать другие события на документе
-//     document.addEventListener('touchmove', swipeAction);
-//     document.addEventListener('touchend', swipeEnd);
-//     document.addEventListener('mousemove', swipeAction);
-//     document.addEventListener('mouseup', swipeEnd);
-//   },
-//   swipeAction = function () {
-//     let evt = getEvent(),
-//       // для более красивой записи возьмем в переменную текущее свойство transform
-//       style = sliderTrack.style.transform,
-//       // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
-//       transform = +style.match(trfRegExp)[0];
-
-//     posX2 = posX1 - evt.clientX;
-//     posX1 = evt.clientX;
-
-//     sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
-//     // можно было бы использовать метод строк .replace():
-//     // sliderTrack.style.transform = style.replace(trfRegExp, match => match - posX2);
-//     // но в дальнейшем нам нужна будет текущая трансформация в переменной
-//   }
-
-// swipeEnd = function () {
-//   // финальная позиция курсора
-//   posFinal = posInit - posX1;
-
-//   document.removeEventListener('touchmove', swipeAction);
-//   document.removeEventListener('mousemove', swipeAction);
-//   document.removeEventListener('touchend', swipeEnd);
-//   document.removeEventListener('mouseup', swipeEnd);
-
-//   // убираем знак минус и сравниваем с порогом сдвига слайда
-//   if (Math.abs(posFinal) > posThreshold) {
-//     // если мы тянули вправо, то уменьшаем номер текущего слайда
-//     if (posInit < posX1) {
-//       slideIndex--;
-//       // если мы тянули влево, то увеличиваем номер текущего слайда
-//     } else if (posInit > posX1) {
-//       slideIndex++;
-//     }
-//   }
-
-//   // если курсор двигался, то запускаем функцию переключения слайдов
-//   if (posInit !== posX1) {
-//     slide();
-//   }
-
-// };
-
-// sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
-
-// slider.addEventListener('touchstart', swipeStart);
-// slider.addEventListener('mousedown', swipeStart);
